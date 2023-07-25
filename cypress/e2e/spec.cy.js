aec528fac7bb55ced36b3f059e003887e84ffe11
ZacMcleod <zacamcleod33@gmail.com>
2023-07-25 11:31:43 -0700

    done

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,27 @@
-describe('template spec', () => {
+describe('cypress lab 1', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/');
+    cy.get('p').should('exist');
+  })
+})
+
+describe('cypress lab 2', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    cy.contains('a.App-link', 'Learn React');
+  })
+})
+
+describe('cypress lab 3', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('button').should('exist');
+  })
+})
+
+describe('cypress lab 4', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('button').click()
   })
 })
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the first p element
         </p>
         <a
           className="App-link"
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <button>Click Me</button>
       </header>
     </div>
   );
